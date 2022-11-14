e9c6d12bff2ad1e69dcae20d9367f099d0d4232c
Volantis <putzstuck@gmail.com>
2022-11-13 23:24:17 -0500

    added carousel.js file and working on logic to dynamically create images from scryfall sdk

---
+++ public/js/carousel.js
@@ -0,0 +1,17 @@
+const Scry = require("scryfall-sdk");
+
+
+async function randomImg() {
+    let randomImages;
+    for (let i = 0; i < 5; i++) {
+    console.log('searching...');
+    const card = await Scry.Cards.random()
+    randomImages = (card.image_uris.art_crop);
+    }
+    console.log(randomImages);
+    const randImg = document.createElement(`<img src='${randomImages}'>`)
+    const carousel = document.getElementById('carouselImages');
+    carousel.appendChild(randImg);
+}
+
+randomImg();
