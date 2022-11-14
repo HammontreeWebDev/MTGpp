9320bdd97f43af892fb98434adc417fdc98ed6ce
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-11-13 21:43:36 -0500

    user can now logout from the decks page

--- public/js/decks.js
+++ public/js/decks.js
@@ -1,5 +1,6 @@
 // name vars
-const createDeck = document.getElementById('create-deck');
+const createDeckBtn = document.getElementById('create-deck');
+const signOutBtn = document.getElementById('sign-out');
 
 // function to create deck
 function createDeckHandler() {
@@ -11,4 +12,21 @@ function createDeckHandler() {
     })
 }
 
-createDeck.addEventListener('click', createDeckHandler);
+// function expression to sign out
+const signOutHandler = async () => {
+    const response = await fetch('/api/user/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+      alert('You have been logged out.')
+    } else {
+      alert('Failed to log out.');
+    }
+  };
+
+// event listners
+createDeckBtn.addEventListener('click', createDeckHandler);
+signOutBtn.addEventListener('click', signOutHandler);
