808aba6734ed768ae8c808a8ee555f6bc65b71c0
Volantis <putzstuck@gmail.com>
2022-11-14 10:13:36 -0500

    added artwork to homepage

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -2,14 +2,11 @@ const Scry = require("scryfall-sdk");
 
 
 async function randomImg() {
-    let randomImages;
+    let randomImages = [];
     for (let i = 0; i < 5; i++) {
     console.log('searching...');
     const card = await Scry.Cards.random()
-    randomImages = (card.image_uris.art_crop);
-    const randImg = document.createElement(`<img src='${randomImages}'>`)
-    const carousel = document.getElementById('carouselImages');
-    carousel.appendChild(randImg);
+    randomImages.push(card.image_uris.art_crop);
     }
 console.log(randomImages);
 }
