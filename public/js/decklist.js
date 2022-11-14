ec6be92d746ac194f0c3fb1a18cc47ae4198aa7b
Brandon <hillbp2014@gmail.com>
2022-11-14 14:13:24 -0500

    autocomplete card search

--- public/js/decklist.js
+++ public/js/decklist.js
@@ -1,10 +1,24 @@
 // require the SDK for scry
-const Scry = require('scryfall-sdk');
+const Scry = require("scryfall-sdk");
 
 // element variables
-let cardSearch = $('#card-search');
+let cardSearch = $("#card-search");
 
 // list cards based on user input
-const results = Scry.Cards.autoCompleteName(`${cardSearch.value}`);
-// logging for now to test
-results.forEach(console.log);
+async function handleAutocomplete(event) {
+    const results = await Scry.Cards.autoCompleteName(`${event.target.value}`);
+
+    // logging for now to test
+    results.forEach(console.log);
+
+    cardSearch.autocomplete({
+        source: results,
+    });
+}
+
+// async function handleSubmit(event) {
+//     const card = await Scry.Cards.byName(event.target.value);
+// }
+
+cardSearch.on("input", handleAutocomplete);
+// cardSearch.on("submit", handleSubmit);
