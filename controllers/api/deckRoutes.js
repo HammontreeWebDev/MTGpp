36881d0b17717bd3514ca80016f6d0a1e5223412
JK code <112551853+jkanvision@users.noreply.github.com>
2022-11-13 23:24:47 -0500

    edits create deck to store user_id

--- controllers/api/deckRoutes.js
+++ controllers/api/deckRoutes.js
@@ -28,8 +28,16 @@ router.get("/:id", async (req, res) => {
 // TODO: Check for authorization
 router.post("/", async (req, res) => {
     try {
-        const deckData = await Deck.create(req.body);
-        res.status(200).json(deckData);
+        const deckData = await Deck.create({
+            user_id: req.session.user_id,
+            deck_name: req.body.deck_name,
+        });
+        req.session.save(() => {
+            req.session.user_id = deckData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(deckData);
+    });
     } catch (err) {
         console.error(err);
         res.status(400).json(err);
