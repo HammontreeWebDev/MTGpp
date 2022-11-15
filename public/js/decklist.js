42253507e7aa138761fe6fa26df8845a4695c1b4
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-11-15 17:35:08 -0500

    able to get card information back from the api using card name when user hits enter

--- public/js/decklist.js
+++ public/js/decklist.js
@@ -1,6 +1,6 @@
 // element variables
 let cardSearch = $("#card-search");
-let searchResults = $("#card-name-results");
+let cardSubmit = $("#card-submit");
 
 // function to handle the autocomplete api call
 function handleAutocomplete() {
@@ -18,5 +18,33 @@ function handleAutocomplete() {
     }
 }
 
+// function / submit handler to show card art when a card is chosen
+cardSubmit.submit(function(event) {
+
+    event.preventDefault();
+        fetch(`../api/scry/name/${$( "input" ).first().val()}`)
+        .then((response) => {
+            // console.log($( "input" ).first().val());
+            if(response.ok) {
+            return response.json();
+            }
+            else{
+                throw new Error(response.status)
+            }
+        })
+        .then((response) => {
+            console.log(response);
+        }
+        )
+        .catch((error) => {
+            console.log(error);
+        }) 
+
+    });
+
+
+// Event Listeners:
 cardSearch.on("input", handleAutocomplete);
 // cardSearch.on("submit", handleSubmit);
+// cardSearch.submit(handleCardArt(event));
+
