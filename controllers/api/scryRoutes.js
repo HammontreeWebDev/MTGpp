e4fd8b60e639017539496fa2a65ffbb24dc018c0
Brandon <hillbp2014@gmail.com>
2022-11-15 16:49:29 -0500

    api collection route

--- controllers/api/scryRoutes.js
+++ controllers/api/scryRoutes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const Scry = require("scryfall-sdk");
+const { User, Deck } = require("../../models");
 
+// Get a card by ID
 router.get("/:id", async (req, res) => {
     try {
         const cardData = await Scry.Cards.byName(req.params.id);
         if (!cardData) {
-            res.status(404).json({ message: "No card found" })
+            res.status(404).json({ message: "No card found" });
         }
         res.status(200).json(cardData);
     } catch (err) {
@@ -14,6 +16,7 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+// Get a random card
 router.get("/random", async (req, res) => {
     try {
         const cardData = await Scry.Cards.random();
@@ -24,6 +27,7 @@ router.get("/random", async (req, res) => {
     }
 });
 
+// Get a card by name
 router.get("/name/:name", async (req, res) => {
     try {
         const cardData = await Scry.Cards.byName(req.params.name);
@@ -37,6 +41,7 @@ router.get("/name/:name", async (req, res) => {
     }
 });
 
+// Autocomplete card by name
 router.get("/autocomplete/:name", async (req, res) => {
     try {
         const autocomplete = await Scry.Cards.autoCompleteName(req.params.name);
@@ -48,6 +53,45 @@ router.get("/autocomplete/:name", async (req, res) => {
         console.error(err);
         res.status(500).json(err);
     }
-})
+});
+
+// Get an array of cards from a deck ID
+router.get("/collection/:id", async (req, res) => {
+    try {
+        // Get deck data from database
+        const deckData = await Deck.findByPk(req.params.id);
+
+        // Does that card exist?
+        if (!deckData) {
+            res.status(404).json({
+                message: `No deck found with id: ${req.params.id}`,
+            });
+            return;
+        }
+
+        // Serialize data
+        const deckList = deckData.get({ plain: true }).deck_list;
+
+        // Create card collection
+        const deckArray = deckList.map((element) =>
+            Scry.CardIdentifier.byId(element.id)
+        );
+
+        // Scryfall collection call
+        const deckCollection = await Scry.Cards.collection(
+            ...deckArray
+        ).waitForAll();
+
+        // Did it work?
+        if (deckCollection) {
+            res.status(200).json(deckCollection);
+        } else {
+            res.status(400).json({ message: "Something weird happened" });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
