be0ddae1a8774480f2a1a557c33857b2182315a9
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-11-14 20:10:39 -0500

    added decklist routes to index.js for api call

--- public/js/decks.js
+++ public/js/decks.js
@@ -3,8 +3,6 @@ const createDeckBtn = document.getElementById('create-deck');
 const signOutBtn = document.getElementById('sign-out');
 const deleteBtn = document.getElementsByClassName('deleteBtn');
 
-
-
 // function to create deck
 async function createDeckHandler() {
   let answer = prompt('Please name your deck:');
@@ -20,7 +18,6 @@ async function createDeckHandler() {
   else {
     console.error(new Error());
   }
-
 }
 
 // function to edit deck contents - this will re-route to decklist
@@ -28,7 +25,13 @@ async function editDeckHandler() {
   const edit = await fetch('api/deck', {
     method: 'GET',
     
-  })
+  });
+  if (edit.ok) {
+    document.location.replace('/decklist');
+  }
+  else {
+    console.error(new Error());
+  }
 }
 
 // function to delete specific deck
