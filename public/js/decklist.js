4fce6bbcf4e6132199acf2476b49b0a965457072
Brandon <hillbp2014@gmail.com>
2022-11-15 11:19:02 -0500

    used jquery ui for autocomplete on frontend

--- public/js/decklist.js
+++ public/js/decklist.js
@@ -4,19 +4,16 @@ let searchResults = $("#card-name-results");
 
 // function to handle the autocomplete api call
 function handleAutocomplete() {
-    fetch(`https://api.scryfall.com/cards/autocomplete?q=${this.value}`)
-        .then(response => {
-            return response.json()
+    fetch(`api/scry/autocomplete/${this.value}`)
+        .then((response) => {
+            return response.json();
         })
-        .then(response => {
+        .then((response) => {
             // console.log(response);
-            for (let i = 0; i < response.data.length; i++) {
-                let cardNames = response.data[i];
-                console.log(cardNames);
-                let button = $(`<button class="result-btn" value="">${cardNames}</button>`)
-                searchResults.append(button);
-            }
-        })
+            cardSearch.autocomplete({
+                source: response,
+            });
+        });
 }
 
 cardSearch.on("input", handleAutocomplete);
